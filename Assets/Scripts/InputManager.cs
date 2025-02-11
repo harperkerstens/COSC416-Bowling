f578521c1f1d2d77be3cf17a86ff7dc3d92e22c9
Harper Kerstens <63266571+harperkerstens@users.noreply.github.com>
2025-02-10 16:19:40 -0800

    Updated InputManager
    added inputmanager to sphere

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+public class InputManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public UnityEvent OnSpacePressed = new UnityEvent();
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnSpacePressed?.Invoke();
+        }
+        
+    }
+}
