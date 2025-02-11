c91afdb0cdc47f218cf2558d77a51fe47e619921
Harper Kerstens <63266571+harperkerstens@users.noreply.github.com>
2025-02-10 17:14:13 -0800

    Added Pins and Created Pins prefab

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,6 +6,7 @@ public class BallController : MonoBehaviour
     [SerializeField] private float force = 1f;
    [SerializeField] private InputManager inputManager;
 
+    private bool isBallLaunched;
     private Rigidbody ballRB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +17,8 @@ public class BallController : MonoBehaviour
     }
 
     private void LaunchBall(){
+        if(isBallLaunched) return;
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 
